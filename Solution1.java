18afbb361d5ddf84172adce2345df3972db87cf4
Rihui Lu <111410716+DiuqsM@users.noreply.github.com>
2024-01-21 23:33:37 -0800

    Update Solution1.java

--- Solution1.java
+++ Solution1.java
@@ -2,11 +2,14 @@ class Solution1 {
     public int removeDuplicates(int[] nums) {
         ArrayList<Integer> map = new ArrayList<Integer>(); 
         int j = 0; 
+        //iterate through the num array skipping and adding it to the ArrayList while skipping the ones that are already in the array list
         for(int i = 0; i < nums.length; i++){
             if(!map.contains(nums[i])){
             map.add(nums[i]); 
             }
         }
+
+        //updates the num augment to the arraylist
         for(int i = 0; i < map.size(); i++){
             nums[i] = map.get(i); 
         }
